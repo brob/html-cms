af1dfdc51290ae4db017001b6248826fea4fee11
Bryan Robinson <bryanlrobinson@gmail.com>
2020-08-25 16:35:25 -0400

    Code Cleanup and refactor

--- utils/SanityClient.js
+++ utils/SanityClient.js
@@ -3,7 +3,7 @@ const client = sanityClient({
     projectId: '4fs6x5jg',
     dataset: 'production',
     token: process.env.SANITY_KEY, // or leave blank to be anonymous user
-    useCdn: false // `false` if you want to ensure fresh data
+    useCdn: true 
   })
 
 module.exports = client;
